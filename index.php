3e7219d2029489cb2cf4b6f36417c6f1c2e10c6c
tallhand <tallhand27@yahoo.com.tw>
2016-03-25 07:57:13 +0800

    finall

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 <html>
 <head>
 	<title>山腳傳奇-回歸傳統</title>
+	<style>
+		.demo6 {
+		text-align: center;
+		background:hsla(20, 100%, 50%, 0.3);
+		color: #654321;
+		width: 200px;
+		height: 120px;
+		line-height: 120px;
+		border-radius: 10px;	/* 高度 */
+		}
+	</style>
 </head>
-<body background="index2.jpg">
+<body background="index3.jpg">
+<div style="padding-left: 100px ;padding-top:20px ;">
+	<span><button class="demo6" onclick="location.href='menu.php'"><strong><font face="標楷體" size="5">產品項目</font></strong></button></span>
+	<br><br><br>
+	<span><button class="demo6" onclick="location.href='shop.php'"><strong><font face="標楷體" size="5">馬上訂購</font></strong></button></span>
+	<br><br><br>
+	<span><button class="demo6" onclick="location.href='about.php'"><strong><font face="標楷體" size="5">關於我們</font></strong></button></span>
+	<br><br><br>
+	<span><button class="demo6" onclick="location.href='history.php'"><strong><font face="標楷體" size="5">山腳歷史</font></strong></button></span>
+</div>
 
-<input type="button" value="產品項目" onclick="location.href='menu.php'">
-
-<input type="button" value="馬上訂購" onclick="location.href='shop.php'">
-
-<input type="button" value="關於我們" onclick="location.href='about.php'">
-
-<input type="button" value="山腳歷史" onclick="location.href='history.php'">
 
 </body>
 </html>
